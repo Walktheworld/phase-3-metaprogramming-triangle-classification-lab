902d6d75218a60baa4fb217fe65886a572ff488b
Joe <Walktheworld@me.com>
2022-02-16 15:33:03 -0700

    commit tree 2c9242e0d3b457875566c000a3d5670a98ea5a14

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  attr_reader :s1, :s2, :s3
+  
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+
+    if s1 >= s2 + s3 || s2 >= s1 + s3 || s3 >= s2 + s1
+      raise TriangleError
+    end
+    if s1 <= 0 || s2 <= 0 || s3 <= 0
+      raise TriangleError
+    end
+  end
+
+  def kind
+    if s1 == s2 && s2 == s3
+      :equilateral
+    elsif s1 == s2 || s2 == s3 || s1 ==s3
+      :isosceles
+    else 
+      :scalene
+    end 
+  end
+
+
+  class TriangleError < StandardError
+    def message 
+      "not a triangle"
+    end
+  end
 end
